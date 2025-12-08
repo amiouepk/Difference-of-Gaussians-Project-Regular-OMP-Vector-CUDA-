b641e60c72b3a7e492439c4e5f635fd9ac3fb1fc
amiouepk <anish1mani@egmail.com>
2025-12-08 14:50:57 -0500

    made some function in the main.cpp that will need to be moved later. Experimenting with the implementation fo the guassian functions

--- main.cpp
+++ main.cpp
@@ -6,13 +6,32 @@
 #include <webp/encode.h>
 
 
+double difference_of_gaussians();
 
+double laplacian_over_gaussian();
+
+double gaussian_function();
 
 
 int main(int argc, char argv[]){
+    
 
 
 
 
     return 0;
 }
+
+double difference_of_gaussians(){
+
+    return 0.0;
+}
+
+double gaussian_function(double a, double x, double b, double c){
+
+    double f_x = a * exp(-1 * (((x - b) * (x - b)) / (2 * c)));
+
+    return f_x;
+}
+
+
